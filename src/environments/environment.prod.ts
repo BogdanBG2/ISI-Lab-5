25f4f3f8f6141c315e073f7683dc9dec50056391
BogdanBG2 <bogdandrei04@gmail.com>
2021-11-10 22:53:04 +0200

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,18 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+export const environment = {
+  firebase: {
+	apiKey: "AIzaSyBnmBZtuDJ3HZ_6ifFDaMjGzBdEMIeP_yU",
+	authDomain: "isi---lab-5.firebaseapp.com",
+	databaseURL: "https://isi---lab-5-default-rtdb.europe-west1.firebasedatabase.app",
+	projectId: "isi---lab-5",
+	storageBucket: "isi---lab-5.appspot.com",
+	messagingSenderId: "71135751350",
+	appId: "1:71135751350:web:c1072845ffba64ad888a17",
+	measurementId: "G-B77Z0Z1RWY"
+  },
+  production: true,
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  firebase: {
+	apiKey: "AIzaSyBnmBZtuDJ3HZ_6ifFDaMjGzBdEMIeP_yU",
+	authDomain: "isi---lab-5.firebaseapp.com",
+	databaseURL: "https://isi---lab-5-default-rtdb.europe-west1.firebasedatabase.app",
+	projectId: "isi---lab-5",
+	storageBucket: "isi---lab-5.appspot.com",
+	messagingSenderId: "71135751350",
+	appId: "1:71135751350:web:c1072845ffba64ad888a17",
+	measurementId: "G-B77Z0Z1RWY"
+  },
+  production: false,
+};
